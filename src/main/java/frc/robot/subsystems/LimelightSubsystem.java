2f0034f21353c73d0748ddafccd84bf407a8927f
TESRobotics539 <team539robotics@trinityes.org>
2024-02-13 15:33:46 -0500

    set up limelight subsystem

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -108,8 +108,39 @@ public class LimelightSubsystem extends SubsystemBase {
   public Pose2d getBotPose2d(){
     return LimelightHelpers.getBotPose2d(networkTableName);
   }
+  public Pose2d getBotPose2d_wpiRed(){
+    return LimelightHelpers.getBotPose2d_wpiRed(networkTableName);
+  }
+  public Pose2d getBotPose2d_wpiBlue(){
+    return LimelightHelpers.getBotPose2d_wpiBlue(networkTableName);
+  }
   public Pose3d getBotPose3d(){
     return LimelightHelpers.getBotPose3d(networkTableName);
+  }
+  public Pose3d getBotPose3d_wpiRed(){
+    return LimelightHelpers.getBotPose3d_wpiRed(networkTableName);
+  }
+  public Pose3d getBotPose3d_wpiBlue(){
+    return LimelightHelpers.getBotPose3d_wpiBlue(networkTableName);
+  }
+  public Pose3d getBotPose3d_TargetSpace(){
+    return LimelightHelpers.getBotPose3d_TargetSpace(networkTableName);
+  }
+  public Pose3d getCameraPose3d_RobotSpace(){
+    return LimelightHelpers.getCameraPose3d_RobotSpace(networkTableName);
+  }
+  public Pose3d gettargetaPose3d_RobotSpace(){
+    return LimelightHelpers.getTargetPose3d_RobotSpace(networkTableName);
+  }
+  public double getTX(){
+    return  LimelightHelpers.getTX(networkTableName);
+  }
+
+  public double getTY(){
+    return LimelightHelpers.getTY(networkTableName);
+  }
+  public double getTA(){
+    return LimelightHelpers.getTA(networkTableName);
   }
 
   /**
@@ -150,130 +181,3 @@ public class LimelightSubsystem extends SubsystemBase {
   }
 
 }
-
-
-// package frc.robot.subsystems;
-
-// import java.security.Key;
-// import java.util.ArrayList;
-
-// import edu.wpi.first.networktables.NetworkTable;
-// import edu.wpi.first.networktables.NetworkTableEntry;
-// import edu.wpi.first.networktables.NetworkTableInstance;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import frc.robot.LimelightHelpers;
-// import edu.wpi.first.math.geometry.Translation3d;
-// import edu.wpi.first.math.geometry.Transform3d;
-// import edu.wpi.first.networktables.NetworkTable;
-// import edu.wpi.first.networktables.NetworkTableEntry;
-// import edu.wpi.first.networktables.NetworkTableInstance;
-
-// public class LimelightSubsystem extends SubsystemBase {
-//   private final NetworkTable m_limelightTable;
-//   private ArrayList<Double> m_targetList;
-//   private final int MAX_ENTRIES = 50;
-//   private final NetworkTableEntry m_isTargetValid, m_led_entry;
-//   NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-//   private final NetworkTableEntry tx;
-//   private final NetworkTableEntry ty;
-//   private final NetworkTableEntry ta;
-//   private final NetworkTableEntry tv;
-//   private final NetworkTableEntry tl;
-//   private final NetworkTableEntry botpose;
-//   private final NetworkTableEntry botpose_wpiblue;
-//   private final NetworkTableEntry botpose_wpired;
-//   private final NetworkTableEntry camerapose_targetspace;
-//   private final NetworkTableEntry targetpose_cameraspace;
-//   private final NetworkTableEntry targetpose_robotspace;
-//   private final NetworkTableEntry botpose_targetspace;
-//   private final NetworkTableEntry camerapose_robotspace;
-//   private final NetworkTableEntry tid;
-  
-
-// //read values periodically
-// double x;
-// double y;
-// double area;
-// double v;
-
-// //post to smart dashboard periodically
-// //SmartDashboard.putNumber("LimelightX", x);
-// //SmartDashboard.putNumber("LimelightY", y);
-// //SmartDashboard.putNumber("LimelightArea", area);
-
-
-//   /**
-//    * Creates a new Vision.
-//    */
-//   public LimelightSubsystem() {
-//     m_limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
-//     m_targetList = new ArrayList<Double>(MAX_ENTRIES);
-//     //m_isTargetValid = ShuffleboardInfo.getInstance().getTargetEntry();
-//     tx = table.getEntry("tx");
-//     ty = table.getEntry("ty");
-//     ta  = table.getEntry("ta");
-//     tv  = table.getEntry("tv");
-//     tl = table.getEntry("tl");
-//     botpose = table.getEntry("botpose");
-//     botpose_wpiblue = table.getEntry("botpose_wpiblue");
-//     botpose_wpired = table.getEntry("botpose_wpired");
-//     camerapose_targetspace = table.getEntry("camerapose_targetspace");
-//     targetpose_cameraspace = table.getEntry("targetpose_cameraspace");
-//     targetpose_robotspace = table.getEntry("targetpose_robotspace");
-//     botpose_targetspace = table.getEntry("botpose_targetspace");
-//     camerapose_robotspace = table.getEntry("camerapose_robotspace");
-//     tid = table.getEntry("tid");
-
-
-//     m_isTargetValid = 
-//     m_led_entry = m_limelightTable.getEntry("ledMode");
-//   }
-
-//   @Override
-//   public void periodic() {
-//     // This method will be called once per scheduler run
-//     x = tx.getDouble(0.0);
-//     y = ty.getDouble(0.0);
-//     area = ta.getDouble(0.0);
-//     v = tv.getDouble(0.0);
-//     SmartDashboard.putNumber("Limelightx", x);
-//     SmartDashboard.putNumber("Limelighty", y);
-//     SmartDashboard.putNumber("LimelightArea", area);
-
-//     m_isTargetValid.setBoolean(isTargetValid());
-
-//     if (m_targetList.size() >= MAX_ENTRIES) {
-//       m_targetList.remove(0);
-//     }
-//     m_targetList.add(area);
-//   }
-
-//   public double getTX() {
-//     return x;
-//   }
-
-//   public double getTA() {
-//     double sum = 0;
-
-//     for (Double num : m_targetList) { 		      
-//       sum += num.doubleValue();
-//     }
-//     return sum/m_targetList.size();
-//   }
-
-//   public boolean isTargetValid() {
-//     return (v == 1.0); 
-//   }
-
-//   public void setLlLedMode(int mode){
-//     m_led_entry.setDouble((mode));
-//   }
-//   public double getTL(){
-//     return tl.getDouble(0.00);
-//   }
-//   /*public double[] getBotPose(){
-//     return botpose.getDoubleArray(null);
-//   }*/
-
-// }
